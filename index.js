b469b048796770bd751c8cfc8ec96835d4c38d4d
Enrique Corona <enrique_cr1990@hotmail.com>
2020-12-05 01:14:43 -0600

    primera version de prueba para subir a azure

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
-
 var app = express();
+
+const PORT = process.env.PORT || 8080;
 
 app.get('/',function(req,res){
     res.send('******* Servidor Node JS');
 });
 
-app.listen(8080,function(){
-    console.log('***** Servidor Node JS levantado con exito');
+app.listen(PORT,function(){
+    console.log('***** Servidor Node JS levantado con exito - port = ' + PORT);
 });
